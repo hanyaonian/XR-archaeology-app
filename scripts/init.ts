ed56abd5c73d28cbfdcdcd3aeb93b34912275886
MichaelHyn <hanyaonian@outlook.com>
2024-05-13 00:29:49 +0800

    fix: support running in remote origin

--- scripts/init.ts
+++ scripts/init.ts
@@ -3,15 +3,9 @@ import fs from "fs/promises";
 import path from "path";
 
 const projectConfig = config();
-<<<<<<< HEAD
 const { appid, developer } = projectConfig?.parsed ?? {};
 
 if (!appid || !developer || projectConfig.error) {
-=======
-const appid = projectConfig?.parsed?.appid;
-
-if (!appid || projectConfig.error) {
->>>>>>> e2fd94b (chore: change expo project id)
   throw new Error(".env or appid not found. Please follow readme file to start.");
 }
 
@@ -20,11 +14,6 @@ if (!appid || projectConfig.error) {
   const appJsonPath = path.resolve(process.cwd(), "app.json");
 
   const content = await fs.readFile(templateAppJsonPath, { encoding: "utf-8" });
-<<<<<<< HEAD
   await fs.writeFile(appJsonPath, content.replace("[PROJECT_ID]", appid).replace("[DEVELOPER]", developer));
-=======
-  await fs.writeFile(appJsonPath, content.replace("[PROJECT_ID]", appid));
-
->>>>>>> e2fd94b (chore: change expo project id)
   console.log('DONE');
 })();
